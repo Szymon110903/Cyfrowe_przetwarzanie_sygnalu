90e810c4c39382af79eac360a7a039a42e8a543a
Filip Wasiel <filipwasiel@gmail.com>
2026-03-04 18:58:28 +0100

    dodanie impulsu jednostkowego i zmiana definicji plotu aby wybieral czy jest dyskretny czy nie

--- plots.py
+++ plots.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
-def plot_signal(t, signal, title="Sygnał", xlabel="Czas (s)", ylabel="Amplituda"):
+def plot_signal(t, signal, title="Sygnał", xlabel="Czas (s)", ylabel="Amplituda", discrete=False):
     plt.figure(figsize=(10, 4))
-    plt.plot(t, signal, label='Sygnał')
+    if discrete:
+        plt.stem(t, signal, label='Sygnał (dyskretny)', basefmt=" ")
+    else:
+        plt.plot(t, signal, label='Sygnał (ciągły)')
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
